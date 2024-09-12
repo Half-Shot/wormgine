42ac970caa6765773448b5b01719f9c84e6353fd
Half-Shot <will@half-shot.uk>
2024-09-12 22:54:19 +0100

    Add new input controller.

---
+++ src/input.ts
@@ -0,0 +1,50 @@
+import { EventEmitter } from "pixi.js";
+
+export enum InputKind {
+    MoveLeft,
+    MoveRight,
+    Fire,
+}
+
+const DefaultBinding: Record<string, InputKind> = Object.freeze({
+    "ArrowLeft": InputKind.MoveLeft,
+    "ArrowRight": InputKind.MoveRight,
+    "Space": InputKind.Fire,
+});
+
+class Controller extends EventEmitter {
+
+    private readonly activeInputs = new Set();
+
+    constructor() {
+        super();
+        window.addEventListener('keydown', this.onKeyDown.bind(this));
+        window.addEventListener('keyup', this.onKeyUp.bind(this));
+    }
+
+    public isInputActive(kind: InputKind) {
+        return this.activeInputs.has(kind);
+    }
+
+    private onKeyDown(ev: KeyboardEvent) {
+        const inputKind = DefaultBinding[ev.key];
+        if (inputKind === undefined || this.activeInputs.has(inputKind)) {
+            return;
+        }
+        this.activeInputs.add(inputKind);
+        this.emit('inputBegin', inputKind);
+    }
+
+    private onKeyUp(ev: KeyboardEvent) {
+        const inputKind = DefaultBinding[ev.key];
+        if (inputKind === undefined || !this.activeInputs.has(inputKind)) {
+            return;
+        }
+        this.activeInputs.delete(inputKind);
+        this.emit('inputEnd', inputKind);
+    }
+}
+
+const staticController = new Controller();
+
+export default staticController;
