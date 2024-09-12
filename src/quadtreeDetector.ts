6bb45935bc3d14a0c15d76affea237e5d442d178
Half-Shot <will@half-shot.uk>
2024-09-12 22:54:33 +0100

    Update quadtree for new matter.js

--- src/quadtreeDetector.ts
+++ src/quadtreeDetector.ts
@@ -17,9 +17,14 @@ export class QuadtreeDetector implements Detector {
     private allBodies: Body[] = [];
 
     private lastCalculatedActiveBodies = 0;
+    private lastRegion?: Rectangle<Body>;
 
     public get activeBodies() {
         return this.lastCalculatedActiveBodies;
+    }
+
+    public get activeRegion() {
+        return this.lastRegion;
     }
 
     public set bodies(newBodies) {
@@ -29,18 +34,20 @@ export class QuadtreeDetector implements Detector {
     }
 
     public get bodies() {
-        const rect = new Rectangle<Body>({ x: 0, y: 0, width: 0, height: 0 });
-        this.allBodies.filter(b => !b.isSleeping).forEach((body) => {
+        const rect = new Rectangle<Body>({ x: 100000, y: 100000, width: 0, height: 0 });
+        this.allBodies.forEach((body) => {
             const width = (body.bounds.max.x - body.bounds.min.x);
             const height = (body.bounds.max.y - body.bounds.min.y);
             const x = body.position.x - width/2;
             const y = body.position.y - height/2;
 
-            rect.x = Math.min(rect.x, x);
-            rect.y = Math.min(rect.y, y);
-            rect.width = Math.max(rect.width, x + width/2);
-            rect.height = Math.max(rect.height, y + height/2);
+            // TODO: This needs revisiting.
+            rect.x = Math.max(0, Math.min(rect.x, x) - 100);
+            rect.y = Math.max(0, Math.min(rect.y, y) - 100);
+            rect.width = Math.max(0, Math.max(rect.width, (x + width/2) - rect.x) + 100);
+            rect.height = Math.max(0, Math.max(rect.height, (y + height/2) - rect.y) + 100);
         });
+        this.lastRegion = rect;
     
         if (rect.x + rect.y + rect.width + rect.height === 0) {
             return [];
