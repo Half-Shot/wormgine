df5a5b44fcb45ec33d5930192ed0e744477a9c36
Will Hunt <will@half-shot.uk>
2024-10-01 02:24:54 +0100

    Add support for fall damage and better weapon firing

--- src/entities/playable/worm.ts
+++ src/entities/playable/worm.ts
@@ -1,5 +1,5 @@
 import {  Sprite, Texture } from 'pixi.js';
-import { IWeaponDefiniton } from '../../weapons/weapon';
+import { FireOpts, IWeaponDefiniton } from '../../weapons/weapon';
 import { WeaponGrenade } from '../../weapons/grenade';
 import Controller, { InputKind } from '../../input';
 import { collisionGroupBitmask, CollisionGroups, GameWorld, PIXELS_PER_METER } from '../../world';
@@ -10,19 +10,29 @@ import { PlayableEntity } from './playable';
 import { WormInstance } from '../../logic/teams';
 import { calculateMovement } from '../../movementController';
 import { Viewport } from 'pixi-viewport';
+import { magnitude } from '../../utils';
 
-enum WormState {
+export enum WormState {
     Idle = 0,
     InMotion = 1,
     Firing = 2,
     MovingLeft = 3,
     MovingRight = 4,
+    Inactive = 5,
 }
 
-type FireWeaponFn = (worm: Worm, definition: IWeaponDefiniton, duration: number) => void;
+export enum EndTurnReason {
+    TimerElapsed = 0,
+    FallDamage = 1,
+    FiredWeaponNoHit = 2,
+    FiredWeaponAndHit = 3,
+    FiredWeaponAndKilled = 3,
+}
 
-const maxWormStep = new MetersValue(0.2);
 
+const maxWormStep = new MetersValue(0.6);
+
+type FireFn = (worm: Worm, selectedWeapon: IWeaponDefiniton, opts: FireOpts) => void;
 
 /**
  * Physical representation of a worm on the map. May be controlled.
@@ -35,13 +45,20 @@ export class Worm extends PlayableEntity {
     }
 
     private static texture: Texture;
+    private static impactDamageMultiplier = 50;
+    private static minImpactForDamage = 0.05;
     private static offsetFromGroundM =  0.04;
 
     private fireWeaponDuration = 0;
     private currentWeapon: IWeaponDefiniton = WeaponGrenade;
     private state: WormState;
+    private turnEndedReason: EndTurnReason|undefined;
+    private impactVelocity = 0;
+    // TODO: Best place for this var?
+    private weaponTimerSecs = 3;
+    public fireAngle = 0;
 
-    static create(parent: Viewport, world: GameWorld, position: Coordinate, wormIdent: WormInstance, onFireWeapon: FireWeaponFn) {
+    static create(parent: Viewport, world: GameWorld, position: Coordinate, wormIdent: WormInstance, onFireWeapon: FireFn) {
         const ent = new Worm(position, world, parent, wormIdent, onFireWeapon);
         world.addBody(ent, ent.physObject.collider);
         parent.addChild(ent.sprite);
@@ -54,8 +71,17 @@ export class Worm extends PlayableEntity {
         return this.physObject.body.translation();
     }
 
-    private constructor(position: Coordinate, world: GameWorld, parent: Viewport, wormIdent: WormInstance, private readonly onFireWeapon: FireWeaponFn) {
+    get currentState() {
+        return this.state;
+    }
+
+    get endTurnReason() {
+        return this.turnEndedReason;
+    }
+
+    private constructor(position: Coordinate, world: GameWorld, parent: Viewport, wormIdent: WormInstance, private readonly onFireWeapon: FireFn) {
         const sprite = new Sprite(Worm.texture);
+        sprite.scale.set(0.5, 0.5);
         sprite.anchor.set(0.5, 0.5);
         const body = world.createRigidBodyCollider(
             ColliderDesc.cuboid(sprite.width / (PIXELS_PER_METER*2), sprite.height / (PIXELS_PER_METER*2))
@@ -68,27 +94,57 @@ export class Worm extends PlayableEntity {
             explosionRadius: new MetersValue(5),
             damageMultiplier: 5,
         });
-        this.state = WormState.InMotion;
+        this.state = WormState.Inactive;
 
         // TODO: Unbind.
-        Controller.on('inputBegin', (inputKind: InputKind) => {
-            if (inputKind === InputKind.MoveLeft || inputKind === InputKind.MoveRight) {
-                this.setMoveDirection(inputKind);
-            } else if (inputKind === InputKind.Fire) {
-                this.onBeginFireWeapon();
-            }
-        });
-
-        Controller.on('inputEnd', (inputKind: InputKind) => {
-            if (inputKind === InputKind.Fire) {
-                this.onEndFireWeapon();
-            }
-            if (inputKind === InputKind.MoveLeft || inputKind === InputKind.MoveRight) {
-                this.resetMoveDirection(inputKind);
-            } 
-        });
-
         //this.onStoppedMoving();
+    }
+
+    onWormSelected() {
+        this.state = WormState.Idle;
+        Controller.on('inputBegin', this.onInputBegin);
+        Controller.on('inputEnd', this.onInputEnd);
+    }
+
+    onEndOfTurn() {
+        Controller.removeListener('inputBegin', this.onInputBegin);
+        Controller.removeListener('inputEnd', this.onInputEnd);
+    }
+
+    onInputBegin = (inputKind: InputKind) => {
+        if (inputKind === InputKind.MoveLeft || inputKind === InputKind.MoveRight) {
+            this.setMoveDirection(inputKind);
+        } else if (inputKind === InputKind.Fire) {
+            this.onBeginFireWeapon();
+        }
+        if (this.currentWeapon.timerAdjustable) {
+            switch(inputKind) {
+                case InputKind.WeaponTimer1:
+                    this.weaponTimerSecs = 1;
+                    break;
+                case InputKind.WeaponTimer2:
+                    this.weaponTimerSecs = 2;
+                    break;
+                case InputKind.WeaponTimer3:
+                    this.weaponTimerSecs = 3;
+                    break;
+                case InputKind.WeaponTimer4:
+                    this.weaponTimerSecs = 4;
+                    break;
+                case InputKind.WeaponTimer5:
+                    this.weaponTimerSecs = 5;
+                    break;
+            }
+        }
+    }
+
+    onInputEnd = (inputKind: InputKind) => {
+        if (inputKind === InputKind.Fire) {
+            this.onEndFireWeapon();
+        }
+        if (inputKind === InputKind.MoveLeft || inputKind === InputKind.MoveRight) {
+            this.resetMoveDirection(inputKind);
+        } 
     }
 
     setMoveDirection(direction: InputKind.MoveLeft|InputKind.MoveRight) {
@@ -97,6 +153,7 @@ export class Worm extends PlayableEntity {
             // Falling, can't move
             return;
         }
+        this.fireAngle = direction === InputKind.MoveLeft ? -1 : 1;
         this.state = direction === InputKind.MoveLeft ? WormState.MovingLeft : WormState.MovingRight;
     }
 
@@ -114,9 +171,9 @@ export class Worm extends PlayableEntity {
         }
     }
 
-    onMove(moveState: WormState.MovingLeft|WormState.MovingRight, dt: number) {
+    onMove(moveState: WormState.MovingLeft|WormState.MovingRight) {
         // Attempt to move to the left or right by 3 pixels
-        const movementMod = 0.06;
+        const movementMod = 0.05;
         const moveMod = new Vector2(moveState === WormState.MovingLeft ? -movementMod : movementMod, 0);
         const move = calculateMovement(this.physObject, moveMod, maxWormStep, this.gameWorld);
         this.physObject.body.setTranslation(move, false);
@@ -156,6 +213,7 @@ export class Worm extends PlayableEntity {
     // }
 
     onBeginFireWeapon() {
+        console.log('BARK!');
         this.state = WormState.Firing;
     }
 
@@ -163,11 +221,15 @@ export class Worm extends PlayableEntity {
         if (this.state !== WormState.Firing) {
             return;
         }
-        this.state = WormState.Idle;
+        // TODO: Need a middle state for while the world is still active.
+        this.state = WormState.Inactive;
+        this.turnEndedReason = EndTurnReason.FiredWeaponNoHit;
         const duration = this.fireWeaponDuration;
         this.fireWeaponDuration = 0;
-        console.log('Weapon fired with a duration of', duration);
-        this.onFireWeapon(this, this.currentWeapon, duration);
+        this.onFireWeapon(this, this.currentWeapon, {
+            duration,
+            timer: this.weaponTimerSecs,
+        });
     }
 
     update(dt: number): void {
@@ -175,6 +237,11 @@ export class Worm extends PlayableEntity {
         if (this.sprite.destroyed) {
             return;
         }
+        if (this.state === WormState.Inactive) {
+            // Do nothing.
+            return;
+        }
+        const falling = this.body.linvel().y > 0.05;
 
         this.wireframe.setDebugText(`worm_state: ${WormState[this.state]}`);
         //this.wireframe.setDebugText(`Worm\n${this.physObject.body.isEnabled() ? "static" : "dynamic"}\nVelocity: ${this.physObject.body.linvel().x.toPrecision(2) } ${this.physObject.body.linvel().y.toPrecision(2)}`);
@@ -185,20 +252,27 @@ export class Worm extends PlayableEntity {
                 this.state = WormState.Idle;
                 // Gravity does not affect us while we are idle.
                 //this.body.setGravityScale(0, false);
-                console.log(this.physObject.body.translation(), this.physObject.collider.translation());
+                if (this.impactVelocity > Worm.minImpactForDamage) {
+                    const damage = this.impactVelocity*Worm.impactDamageMultiplier;
+                    this.health -= damage;
+                    this.state = WormState.Inactive;
+                    this.turnEndedReason = EndTurnReason.FallDamage;
+                }
+                this.impactVelocity = 0;
             }
-            // We're in motion, either we've been blown up, falling, or otherwise not in control.
-            // if (this.body.isSleeping) {
-            //     this.onStoppedMoving();
-            // }
         } else if (this.state === WormState.Firing) {
-            if (this.fireWeaponDuration > this.currentWeapon.maxDuration) {
+            if (!this.currentWeapon.maxDuration) {
+                this.onEndFireWeapon();
+            } else if (this.fireWeaponDuration > this.currentWeapon.maxDuration) {
                 this.onEndFireWeapon();
             } else {
                 this.fireWeaponDuration += dt;
             }
+        } else if (falling) {
+            this.state = WormState.InMotion;
+            this.impactVelocity = Math.max(magnitude(this.body.linvel()), this.impactVelocity);
         } else if (this.state === WormState.MovingLeft || this.state === WormState.MovingRight) {
-            this.onMove(this.state, dt);
+            this.onMove(this.state);
         } // else, we're idle and not currently moving.
     }
 
