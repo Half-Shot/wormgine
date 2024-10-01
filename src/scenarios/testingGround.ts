034825583a0e7f3203998c9ac79a9ecbbed50416
Will Hunt <will@half-shot.uk>
2024-10-01 02:26:02 +0100

    Add support for basic round mechanics in testing ground

--- src/scenarios/testingGround.ts
+++ src/scenarios/testingGround.ts
@@ -4,10 +4,12 @@ import { BitmapTerrain } from "../entities/bitmapTerrain";
 import type { Game } from "../game";
 import { Water } from "../entities/water";
 import { Grenade } from "../entities/phys/grenade";
-import { Worm } from "../entities/playable/worm";
+import { EndTurnReason, Worm, WormState } from "../entities/playable/worm";
 import { Coordinate, MetersValue } from "../utils/coodinate";
 import { GameState } from "../logic/gamestate";
-import { TeamGroup } from "../logic/teams";
+import { TeamGroup, teamGroupToColorSet } from "../logic/teams";
+import { GameStateOverlay } from "../overlays/gameStateOverlay";
+import { templateRandomText, TurnEndTextFall, TurnEndTextMiss, TurnEndTextOther, TurnStartText } from "../text/toasts";
 
 export default async function runScenario(game: Game) {
     const parent = game.viewport;
@@ -42,10 +44,13 @@ export default async function runScenario(game: Game) {
             MetersValue.fromPixels(worldHeight), 
         world)
     );    water.addToWorld(parent, world);
-    const worm = world.addEntity(await Worm.create(parent, world, Coordinate.fromScreen(900,105), gameState.getTeamByIndex(0).worms[0], (worm, definition, duration) => {
-        const newProjectile = definition.fireFn(parent, world, worm, duration);
+    const worm = world.addEntity(await Worm.create(parent, world, Coordinate.fromScreen(900,105), gameState.getTeamByIndex(0).worms[0], (worm, definition, opts) => {
+        const newProjectile = definition.fireFn(parent, world, worm, opts);
         world.addEntity(newProjectile);
     }));
+
+
+    const overlay = new GameStateOverlay(game.pixiApp.ticker, game.pixiApp.stage, gameState, world, game.viewport.screenWidth, game.viewport.screenHeight);
 
     game.viewport.follow(worm.sprite);
 
@@ -55,5 +60,42 @@ export default async function runScenario(game: Game) {
         //const entity = await BazookaShell.create(game, parent, composite, position, 1, 0.01, 6);
         world.addEntity(entity);
     });
-    
+
+    let endOfRoundWaitDuration: number|null = 0;
+
+    game.pixiApp.ticker.add((dt) => {
+        if (worm.currentState !== WormState.Inactive) {
+            return;
+        }
+        if (endOfRoundWaitDuration === null) {
+            let reasonSet: string[] = TurnEndTextOther;
+            if (worm.endTurnReason === EndTurnReason.FallDamage) {
+                reasonSet = TurnEndTextFall;
+            } else if (worm.endTurnReason === EndTurnReason.FiredWeaponNoHit) {
+                reasonSet = TurnEndTextMiss;
+            }
+            overlay.addNewToast(templateRandomText(reasonSet, {
+                WormName: worm.wormIdent.name,
+                TeamName: worm.wormIdent.team.name,
+            })  , 2000);
+            // Turn just ended.
+            endOfRoundWaitDuration = 3000;
+            return;
+        }
+        if (endOfRoundWaitDuration <= 0) {
+            const nextState = gameState.advanceRound();
+            if ('winningTeams' in nextState) {
+                console.log('Winner was', nextState);
+            } else {
+                worm.onWormSelected();
+                overlay.addNewToast(templateRandomText(TurnStartText, {
+                    WormName: nextState.nextWorm.name,
+                    TeamName: nextState.nextTeam.name,
+                }), 3000, teamGroupToColorSet(nextState.nextTeam.group).fg);
+            }
+            endOfRoundWaitDuration = null;
+            return;
+        }
+        endOfRoundWaitDuration -= dt.deltaMS;
+    });
 }
