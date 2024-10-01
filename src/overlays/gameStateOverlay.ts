06620cf26cdbd3da763a559a4e83a782b0d48ed1
Will Hunt <will@half-shot.uk>
2024-10-01 02:25:40 +0100

    Add support for "toasts"

--- src/overlays/gameStateOverlay.ts
+++ src/overlays/gameStateOverlay.ts
@@ -1,4 +1,4 @@
-import { Container, Graphics, Text, Ticker, UPDATE_PRIORITY } from "pixi.js";
+import { ColorSource, Container, Graphics, Text, Ticker, UPDATE_PRIORITY } from "pixi.js";
 import { GameState } from "../logic/gamestate";
 import { applyGenericBoxStyle } from "../mixins/styles";
 import { teamGroupToColorSet } from "../logic/teams";
@@ -9,11 +9,16 @@ export class GameStateOverlay {
     public readonly physicsSamples: number[] = [];
     private readonly roundTimer: Text;
     private readonly tickerFn: (dt: Ticker) => void;
+    private readonly toastGfx: Graphics;
     private readonly gfx: Graphics;
     private previousStateIteration = -1;
     private visibleTeamHealth: Record<string, number> = {};
     private healthChangeTensionTimer: number|null = null;
     private readonly largestHealthPool: number;
+
+    private toastTime = 0;
+    private toaster: {text: string, timer: number, color: ColorSource}[] = [];
+    private readonly toastBox: Text;
 
     constructor(
         private readonly ticker: Ticker,
@@ -23,6 +28,7 @@ export class GameStateOverlay {
         private readonly screenWidth: number,
         private readonly screenHeight: number,
     ) {
+        const topY = this.screenHeight / 20;
         this.roundTimer = new Text({
             text: '60',
             style: {
@@ -32,11 +38,25 @@ export class GameStateOverlay {
                 align: 'center',
             },
         });
+        this.toastBox = new Text({
+            text: '',
+            style: {
+                fontFamily: 'Arial',
+                fontSize: 24,
+                fill: 0xFFFFFF,
+                align: 'center',
+            },
+        });
+        this.toastBox.position.set(this.screenWidth / 2, topY);
+        this.toastBox.anchor.set(0.5, 0.5);
         this.roundTimer.position.set((this.screenWidth / 30) + 14, ((this.screenHeight / 10) * 9) + 12);
         this.gfx = new Graphics();
-        this.tickerFn = this.update.bind(this);
+        this.toastGfx = new Graphics();
         this.stage.addChild(this.gfx);
         this.stage.addChild(this.roundTimer);
+        this.stage.addChild(this.toastGfx);
+        this.stage.addChild(this.toastBox);
+        this.tickerFn = this.update.bind(this);
         this.ticker.add(this.tickerFn, undefined, UPDATE_PRIORITY.UTILITY);
         this.largestHealthPool = this.gameState.getTeams().reduceRight((value, team) => Math.max(value, team.maxHealth) , 0);
         this.gameState.getActiveTeams().forEach((t) => {
@@ -44,13 +64,48 @@ export class GameStateOverlay {
         });
     }
 
+    /**
+     * Adds some text to be displayed at the top of the screen.
+     * @param text 
+     * @param timer 
+     */
+    public addNewToast(text: string, timer = 5000, color: ColorSource = '#FFFFFF') {
+        this.toaster.splice(0,0, { text, timer, color });
+    }
+
     private update(dt: Ticker) {
+        const centerX = this.screenWidth / 2;
         if (this.healthChangeTensionTimer && !this.gameWorld.areEntitiesMoving()) {
             this.healthChangeTensionTimer -= dt.deltaTime;
         }
 
         const shouldChangeTeamHealth = this.healthChangeTensionTimer !== null && this.healthChangeTensionTimer <= 0;
 
+        if (!this.toastBox.text) {
+            const newToast = this.toaster.pop();
+            if (newToast) {
+                this.toastGfx.clear();
+                this.toastBox.text = newToast.text;
+                this.toastBox.style.fill = newToast.color;
+                this.toastTime = newToast.timer;
+                this.toastGfx.position.set(
+                    (this.screenWidth / 2) - (this.toastBox.width /2) - 3,
+                    (this.screenHeight / 20) - (this.toastBox.height/2) - 2,
+                )
+                applyGenericBoxStyle(this.toastGfx).roundRect(0, 0, this.toastBox.width + 6, this.toastBox.height + 4, 4).stroke().fill();
+            }
+        }
+
+
+        if (this.toastBox.text) {
+            // Render toast
+            this.toastTime -= dt.deltaMS;
+            if (this.toastTime <= 0) {
+                this.toastBox.text = '';
+                this.toastTime = 0;
+                this.toastGfx.clear();
+            }
+        }
         
         if (this.previousStateIteration === this.gameState.iteration && !shouldChangeTeamHealth) {
             return;
@@ -74,7 +129,6 @@ export class GameStateOverlay {
 
 
         this.roundTimer.text = Math.floor(this.gameState.roundTimer/1000);
-        const centerX = this.screenWidth / 2;
         const bottomY = (this.screenHeight / 10) * 9;
         this.gfx.clear();
 
