01fbce8a4205b99e69f8729e6863d762b3839fd7
Half-Shot <will@half-shot.uk>
2024-10-15 22:12:50 +0100

    Add simple login mechanism

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,15 +2,45 @@ import { useCallback, useEffect, useState } from "preact/hooks";
 import { IngameView } from "./ingame-view";
 import { Menu } from "./menu";
 import { loadAssets } from "../assets";
+import { NetGameClient, NetGameInstance } from "../net/client";
+import { Lobby } from "./lobby";
 
 interface LoadGameProps {
     level: string;
+    gameInstance?: NetGameInstance;
 }
+
 
 export function App() {
     const [gameState, setGameState] = useState<LoadGameProps>();
     const [assetProgress, setAssetProgress] = useState(0);
     const [assetsLoaded, setAssetsLoaded] = useState(false);
+    const [showLobby, setShowLobby] = useState(false);
+    const [client, setClient]= useState<NetGameClient>();
+    const lobbyGameRoomId = new URLSearchParams(window.location.search)?.get('gameRoomId') ?? undefined;
+    const [clientReady, setClientReady] = useState(client?.ready);
+    const [clientShouldReload, setClientShouldReload] = useState(true);
+
+    useEffect(() => {
+        if (!clientShouldReload) {
+            return;
+        }
+        if (client) {
+            return;
+        }
+        setClientShouldReload(false);
+        console.log('Starting client');
+        // Load client.
+        const configStr = localStorage.getItem('wormgine_client_config');
+        if (configStr) {
+            const client = new NetGameClient(JSON.parse(configStr));
+            client.once('sync', () => {
+                setClientReady(true);
+            });
+            setClient(client);
+            client.start();
+        }
+    }, [clientShouldReload]);
 
     const onGoToMenu = useCallback(() => {
         // TODO: Show a win screen!
@@ -21,14 +51,47 @@ export function App() {
         void loadAssets((v) => { setAssetProgress(v) }).then(() => setAssetsLoaded(true));
     }, [setAssetProgress]);
 
+    useEffect(() => {
+        if (client && lobbyGameRoomId && clientReady && !gameState) {
+            // Now we can load into the game.
+            setShowLobby(true);
+        }
+    })
+
+    const onNewGame = useCallback((level: string) => {
+        if (level === 'lobbyGame') {
+            // TODO: Bit of a hack
+            setShowLobby(true);
+        } else {
+            setGameState({level});
+        }
+    }, [setGameState]);
+ 
+    const onLobbyGameStarted = useCallback((instance: NetGameInstance) => {
+        setGameState({level: 'netGame', gameInstance: instance});
+        setShowLobby(false);
+    }, [setGameState]);
+ 
     if (!assetsLoaded) {
         return <main>
             Loading <progress value={assetProgress*100} min={0} max={100}></progress> {Math.round(assetProgress*100)}%
         </main>;
     }
-    if (gameState) {
-        return <IngameView level={gameState.level} onGoToMenu={onGoToMenu} />
+
+    if (lobbyGameRoomId && !clientReady) {
+        return <main>
+            Waiting for client to be ready.
+        </main>;
     }
 
-    return <Menu onNewGame={(level) => setGameState({level})}/>
+    if (showLobby) {
+        return <Lobby client={client} gameRoomId={lobbyGameRoomId} onOpenIngame={onLobbyGameStarted}/>
+    }
+
+    if (gameState) {
+        return <IngameView level={gameState.level} onGoToMenu={onGoToMenu} gameInstance={gameState.gameInstance}/>
+    }
+
+
+    return <Menu onNewGame={onNewGame} reloadClient={() => setClientShouldReload(true)} client={client}/>
 }
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,22 +1,25 @@
-import { useCallback } from "preact/hooks";
+import { useCallback, useState } from "preact/hooks";
 import { ChangelogModal } from "./changelog";
 import "./menu.css";
+import { NetClientConfig, NetGameClient } from "../net/client";
 
 interface Props {
-    onNewGame: (level: string) => void
+    onNewGame: (level: string) => void,
+    reloadClient: () => void,
+    client?: NetGameClient,
 }
 
 const buildNumber = import.meta.env.VITE_BUILD_NUMBER;
 const buildCommit = import.meta.env.VITE_BUILD_COMMIT;
+const DEFAULT_HOMESERVER = import.meta.env.VITE_DEFAULT_HOMESERVER;
 const lastCommit = localStorage.getItem('wormgine_last_commit');
 
-export function Menu({onNewGame}: Props) {
+enum CurrentMenu {
+    MainMenu,
+    AccountMenu,
+}
 
-    const onStartNewGame = useCallback((level: string) => {
-        localStorage.setItem("wormgine_last_commit", buildCommit);
-        onNewGame(level);
-    }, [onNewGame]);
-
+function mainMenu(onStartNewGame: (level: string) => void, setCurrentMenu: (menu: CurrentMenu) => void, clientReady?: boolean) {
     return <main className="menu">
         <h1>Wormgine Debug Build</h1>
         <p>
@@ -42,6 +45,12 @@ export function Menu({onNewGame}: Props) {
                 <button onClick={() => onStartNewGame("replayTesting")}>Test gameplay replay</button>
             </li>
             <li>
+                <button onClick={() => setCurrentMenu(CurrentMenu.AccountMenu)}>Manage Account</button>
+            </li>
+            <li>
+                <button disabled={!clientReady} onClick={() => onStartNewGame("lobbyGame")}>Start new network game</button> (Requires configured client)
+            </li>
+            <li>
                 <button onClick={() => onStartNewGame("boneIsles")}>Bone Isles</button>
             </li>
             <li>
@@ -56,4 +65,72 @@ export function Menu({onNewGame}: Props) {
             <a href="https://github.com/Half-Shot/wormgine/blob/main/src/assets/ASSETS.md">Assets are used under various licences</a>
         </p>
     </main>;
+}
+
+function accountMenu(client: NetGameClient|undefined, setCurrentMenu: (menu: CurrentMenu) => void, reloadClient: () => void) {
+    const [loginInProgress, setLoginInProgress] = useState(false);
+    const [error, setError] = useState<string>();
+    const onSubmit = useCallback(async (e: SubmitEvent) => {
+        e.preventDefault();
+        try {
+            setLoginInProgress(true);
+            const target = e.target as HTMLFormElement;
+            const username = (target.elements.namedItem('username') as HTMLInputElement).value;
+            const password = (target.elements.namedItem('password') as HTMLInputElement).value;
+            const { accessToken } = await NetGameClient.login(DEFAULT_HOMESERVER!, username, password);
+            localStorage.setItem('wormgine_client_config', JSON.stringify({
+                accessToken,
+                baseUrl: DEFAULT_HOMESERVER,
+            } as NetClientConfig));
+            reloadClient();
+        } catch (ex) {
+            setError(ex.toString());
+        } finally {
+            setLoginInProgress(false);
+        }
+    }, [reloadClient]);
+
+    return <main className="menu">
+        <h1>Wormgine Account</h1>
+        <button onClick={() => setCurrentMenu(CurrentMenu.MainMenu)}>Go Back</button>
+        <p>
+            You may log into an existing account below by specifying a username and password. Future versions will allow you to 
+            create an account / login to other servers.
+        </p>
+        {client?.ready === false && <p>Account information is stored and in the progress of connecting.</p>}
+        {client?.ready === true && <p>You are logged in as {client.client.getUserId()}.</p>}
+        {!client && !DEFAULT_HOMESERVER  && <p>No homeserver defined in config.</p>}
+        {!client && DEFAULT_HOMESERVER && <section>
+            {error && <p>Error: <span>{error}</span></p>}
+            <form disabled={loginInProgress} onSubmit={onSubmit}>
+                <input type="text" placeholder="username" id="username"></input>
+                <input type="password" placeholder="password" id="password"></input>
+                <button type="submit">Login</button>
+            </form>
+        </section>
+        }
+    </main>;
+}
+
+
+export function Menu({onNewGame, client, reloadClient}: Props) {
+    const [currentMenu, setCurrentMenu] = useState(CurrentMenu.MainMenu);
+    const [clientReady, setClientReady] = useState(client?.ready);
+
+    client?.once('sync', () => {
+        setClientReady(true);
+    })
+
+    const onStartNewGame = useCallback((level: string) => {
+        localStorage.setItem("wormgine_last_commit", buildCommit);
+        onNewGame(level);
+    }, [onNewGame]);
+
+    if (currentMenu === CurrentMenu.MainMenu) {
+        return mainMenu(onStartNewGame, setCurrentMenu, clientReady);
+    } else if (currentMenu === CurrentMenu.AccountMenu) {
+        return accountMenu(client, setCurrentMenu, reloadClient);
+    }
+
+    return null;
 }
