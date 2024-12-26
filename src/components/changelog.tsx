d5e365be490afe4590bef7eb6fc016cff72a2a84
Will Hunt <will@half-shot.uk>
2024-12-26 12:31:59 +0000

    fix showing changelog

--- src/components/changelog.tsx
+++ src/components/changelog.tsx
@@ -74,7 +74,7 @@ export function ChangelogModal({
 
   let newChangesButton = null;
 
-  if (latestChanges?.length) {
+  if (hasNewBuild && latestChanges?.length) {
     newChangesButton = <button onClick={onClick}>See what's new!</button>;
   }
 
