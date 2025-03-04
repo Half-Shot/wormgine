c7f8c52e5145d5789f922ee95ec30a5a39e1b6d8
Half-Shot <will@half-shot.uk>
2025-03-04 00:31:16 +0000

    fixup tests and add toast caclculation test

--- spec/unit/logic/gamestate.test.ts
+++ spec/unit/logic/gamestate.test.ts
@@ -3,8 +3,8 @@ import { TeamDefinition, TeamGroup, WormIdentity } from "../../../src/logic/team
 import { GameRules, GameState } from "../../../src/logic/gamestate";
 import { GameWorld } from "../../../src/world";
 import { DefaultWeaponSchema } from "../../../src/weapons/schema";
-import Logger, { LogLevels } from "../../../src/log";
 import { of } from "rxjs";
+import { FireResultHitEnemy, templateRandomText, templateText } from "../../../src/text/toasts";
 
 const DEAD_WORM: WormIdentity = {
   name: "fishbait",
@@ -90,97 +90,115 @@ describe('GameState', () => {
     const teams = gameState.getActiveTeams();
     expect(teams).toHaveLength(1);
   });
-  test('should advance round to the next team', () => {
-    const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
-    const [redTeam, blueTeam] = gameState.getActiveTeams();
-    expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[0] });
-    expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
-  });
-  test('should advance round to the next team, skipping over the same group', () => {
-    const gameState = new GameState([RED_TEAM, RED_TEAM_2, BLUE_TEAM], world, DefaultRules);
-    const [redTeam, redTeam2, blueTeam] = gameState.getActiveTeams();
-    expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[0] });
-    expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
-    expect(completeRound(gameState)).toEqual({ nextTeam: redTeam2, nextWorm: redTeam2.worms[0] });
-  });
-  test('should advance round to the next team, should ensure that all teams within the same group get to play', () => {
-    const gameState = new GameState([RED_TEAM, RED_TEAM_2, BLUE_TEAM], world, DefaultRules);
-    const [redTeam, redTeam2, blueTeam] = gameState.getActiveTeams();
-    expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[0] });
-    expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
-    expect(completeRound(gameState)).toEqual({ nextTeam: redTeam2, nextWorm: redTeam2.worms[0] });
-    expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
-    expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[1] });
-  });
-  test('should detect a win when only one group has active worms', () => {
-    const gameState = new GameState([RED_TEAM, RED_TEAM_2, BLUE_TEAM], world, { winWhenOneGroupRemains: true, wormHealth: 100, ammoSchema: DefaultWeaponSchema });
-    const [redTeam, redTeam2, blueTeam] = gameState.getActiveTeams();
-    completeRound(gameState);
-    // Kill the blues.
-    blueTeam.worms[0].setHealth(0);
-    // expect(gameState.advanceRound()).toEqual({ winningTeams: [redTeam, redTeam2] });
-  });
 
-  test('should handle the first round', () => {
-    const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
-    gameState.advanceRound();
-    expect(gameState.iteration).toEqual(1);
-    // Not in preround yet.
-    expect(gameState.isPreRound).toEqual(false);
-    gameState.beginRound();
-    expect(gameState.isPreRound).toEqual(true);
-    expect(gameState.remainingRoundTime).toEqual(5000);
-  });
+  describe('Round progression', () => {
+    test('should advance round to the next team', () => {
+      const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
+      const [redTeam, blueTeam] = gameState.getActiveTeams();
+      expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[0] });
+      expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
+    });
+    test('should advance round to the next team, skipping over the same group', () => {
+      const gameState = new GameState([RED_TEAM, RED_TEAM_2, BLUE_TEAM], world, DefaultRules);
+      const [redTeam, redTeam2, blueTeam] = gameState.getActiveTeams();
+      expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[0] });
+      expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
+      expect(completeRound(gameState)).toEqual({ nextTeam: redTeam2, nextWorm: redTeam2.worms[0] });
+    });
+    test('should advance round to the next team, should ensure that all teams within the same group get to play', () => {
+      const gameState = new GameState([RED_TEAM, RED_TEAM_2, BLUE_TEAM], world, DefaultRules);
+      const [redTeam, redTeam2, blueTeam] = gameState.getActiveTeams();
+      expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[0] });
+      expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
+      expect(completeRound(gameState)).toEqual({ nextTeam: redTeam2, nextWorm: redTeam2.worms[0] });
+      expect(completeRound(gameState)).toEqual({ nextTeam: blueTeam, nextWorm: blueTeam.worms[0] });
+      expect(completeRound(gameState)).toEqual({ nextTeam: redTeam, nextWorm: redTeam.worms[1] });
+    });
+    test('should detect a win when only one group has active worms', () => {
+      const gameState = new GameState([RED_TEAM, RED_TEAM_2, BLUE_TEAM], world, { winWhenOneGroupRemains: true, wormHealth: 100, ammoSchema: DefaultWeaponSchema });
+      const [redTeam, redTeam2, blueTeam] = gameState.getActiveTeams();
+      completeRound(gameState);
+      // Kill the blues.
+      blueTeam.worms[0].setHealth(0);
+      // expect(gameState.advanceRound()).toEqual({ winningTeams: [redTeam, redTeam2] });
+    });
 
-  test('should handle the player moving during preround', () => {
-    const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
-    gameState.advanceRound();
-    expect(gameState.iteration).toEqual(1);
-    gameState.beginRound();
-    gameState.playerMoved();
-    expect(gameState.isPreRound).toEqual(false);
-    expect(gameState.paused).toEqual(false);
-    expect(gameState.remainingRoundTime).toEqual(DefaultRules.roundDurationMs);
-  });
+    test('should handle the first round', () => {
+      const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
+      gameState.advanceRound();
+      expect(gameState.iteration).toEqual(1);
+      // Not in preround yet.
+      expect(gameState.isPreRound).toEqual(false);
+      gameState.beginRound();
+      expect(gameState.isPreRound).toEqual(true);
+      expect(gameState.remainingRoundTime).toEqual(5000);
+    });
 
-  test('should handle preround timing out', () => {
-    const gameState = new GameState([RED_TEAM, BLUE_TEAM], {
-      entitiesMoving$: of(false),
-    } as Partial<GameWorld> as any, DefaultRules);
-    gameState.advanceRound();
-    expect(gameState.iteration).toEqual(1);
-    gameState.beginRound();
-    for (let index = 0; index < 5; index++) {
-      gameState.update({ deltaMS: 1000 });
-    }
-    expect(gameState.remainingRoundTime).toEqual(DefaultRules.roundDurationMs);
-    expect(gameState.isPreRound).toEqual(false);
-    expect(gameState.paused).toEqual(false);
-  });
+    test('should handle the player moving during preround', () => {
+      const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
+      gameState.advanceRound();
+      expect(gameState.iteration).toEqual(1);
+      gameState.beginRound();
+      gameState.playerMoved();
+      expect(gameState.isPreRound).toEqual(false);
+      expect(gameState.paused).toEqual(false);
+      expect(gameState.remainingRoundTime).toEqual(DefaultRules.roundDurationMs);
+    });
 
-  test('should handle round finishing due to timeout', () => {
-    const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
-    const [_redTeam, blueTeam] = gameState.getActiveTeams();
-    gameState.advanceRound();
-    expect(gameState.iteration).toEqual(1);
-    gameState.beginRound();
-    for (let index = 0; index < 5; index++) {
-      gameState.update({ deltaMS: 1000 });
-    }
-    expect(gameState.remainingRoundTime).toEqual(DefaultRules.roundDurationMs);
-    expect(gameState.isPreRound).toEqual(false);
-    expect(gameState.paused).toEqual(false);
-    for (let index = 0; index < 45; index++) {
-      gameState.update({ deltaMS: 1000 });
-    }
-    expect(gameState.remainingRoundTime).toEqual(0);
-    const state = gameState.advanceRound();
-    if ('winningTeams' in state) {
-      throw Error('Unexpected win');
-    }
-    const { nextTeam, nextWorm } = state;
-    expect(nextTeam).toEqual(blueTeam);
-    expect(nextWorm).toEqual(blueTeam.worms[0]);
-  });
+    test('should handle preround timing out', () => {
+      const gameState = new GameState([RED_TEAM, BLUE_TEAM], {
+        entitiesMoving$: of(false),
+      } as Partial<GameWorld> as any, DefaultRules);
+      gameState.advanceRound();
+      expect(gameState.iteration).toEqual(1);
+      gameState.beginRound();
+      for (let index = 0; index < 5; index++) {
+        gameState.update({ deltaMS: 1000 });
+      }
+      expect(gameState.remainingRoundTime).toEqual(DefaultRules.roundDurationMs);
+      expect(gameState.isPreRound).toEqual(false);
+      expect(gameState.paused).toEqual(false);
+    });
 
+    test('should handle round finishing due to timeout', () => {
+      const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
+      const [_redTeam, blueTeam] = gameState.getActiveTeams();
+      gameState.advanceRound();
+      expect(gameState.iteration).toEqual(1);
+      gameState.beginRound();
+      for (let index = 0; index < 5; index++) {
+        gameState.update({ deltaMS: 1000 });
+      }
+      expect(gameState.remainingRoundTime).toEqual(DefaultRules.roundDurationMs);
+      expect(gameState.isPreRound).toEqual(false);
+      expect(gameState.paused).toEqual(false);
+      for (let index = 0; index < 45; index++) {
+        gameState.update({ deltaMS: 1000 });
+      }
+      expect(gameState.remainingRoundTime).toEqual(0);
+      const state = gameState.advanceRound();
+      if ('winningTeams' in state) {
+        throw Error('Unexpected win');
+      }
+      const { nextTeam, nextWorm } = state;
+      expect(nextTeam).toEqual(blueTeam);
+      expect(nextWorm).toEqual(blueTeam.worms[0]);
+    });
+  });
+  describe('Toast calculation', () => {
+    test.only('should show a toast when the enemy was hit', () => {
+      const gameState = new GameState([RED_TEAM, BLUE_TEAM], world, DefaultRules);
+      const [redTeam, blueTeam] = gameState.getActiveTeams();
+      gameState.advanceRound();
+      gameState.beginRound();
+      gameState.markAsFinished();
+      // Take damage
+      blueTeam.worms[0].setHealth(50);
+      const { toast } = gameState.advanceRound();
+      expect(FireResultHitEnemy.map(v => templateText(v, {
+        WormName: redTeam.worms[0].name,
+        TeamName: redTeam.name,
+      }))).toContain(toast);
+    });
+  });
 });
