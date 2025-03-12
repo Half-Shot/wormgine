23caa0a03286e121cb27d8ad6fd24c11a079a502
Half-Shot <will@half-shot.uk>
2025-03-12 13:12:43 +0000

    Add more helper functions to flag console

--- src/flags.ts
+++ src/flags.ts
@@ -1,5 +1,7 @@
 import { EventEmitter } from "pixi.js";
 import Input, { InputKind } from "./input";
+import { WormInstance } from "./logic";
+import { ViewportCamera } from "./camera";
 
 export enum DebugLevel {
   None = 0,
@@ -11,6 +13,8 @@ class Flags extends EventEmitter {
   public DebugView: DebugLevel;
   public simulatePhysics = true;
   public stepAnimationsId = "";
+  public wormInstances?: WormInstance[];
+  public viewportCamera?: ViewportCamera;
 
   constructor() {
     super();
@@ -34,6 +38,11 @@ class Flags extends EventEmitter {
         (this.simulatePhysics = !this.simulatePhysics),
       stepAnimation: (step = true) =>
         (this.stepAnimationsId = step ? Math.random().toString() : ""),
+      getWorm: (nameOrId: string) =>
+        this.wormInstances?.find(
+          (w) => w.name === nameOrId || w.uuid === nameOrId,
+        ),
+      getCamera: () => this.viewportCamera,
     };
   }
 }
